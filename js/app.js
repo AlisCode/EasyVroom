2408e7b9ebfef5e460427db39f8705fa75624f4f
Olivier PINON <pinono@iut-acy.local>
2017-03-17 13:00:41 +0100

    advanced offers

--- js/app.js
+++ js/app.js
@@ -5,4 +5,18 @@ $(function() {
 		$(".anim-car").replaceWith($(".anim-car").clone(true));
 	}, 15000);
 
+	$("#btn-values").on("click", function() {
+		$(document.body).animate({'scrollTop': $("#nos-valeurs").offset().top}, 1000);
+	});
+
+	$("#btn-offers").on("click", function() {
+		$(document.body).animate({'scrollTop': $("#nos-offres").offset().top}, 1000);
+	});
+
+	$("#btn-demo").on("click", function() {
+		$(document.body).animate({'scrollTop': $("#nos-solutions").offset().top}, 1000);
+	});
+
+
+
 });
